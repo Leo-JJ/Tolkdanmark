af6d0cb667dd345d155a012e5dca8bbb6953c789
Jiahua <freshyz@hotmail.com>
2016-10-17 12:35:31 +0200

    Ændring, Klientnavn, timer, og tolkforbindelse, ydelsensomfang og type.

--- app/src/main/java/com/tolkdanmarktolkapp/zeshan/tolkdanmark/Fragmenter/Tolkbilag2_fragment.java
+++ app/src/main/java/com/tolkdanmarktolkapp/zeshan/tolkdanmark/Fragmenter/Tolkbilag2_fragment.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tolkdanmarktolkapp.zeshan.tolkdanmark.R;
 import com.tolkdanmarktolkapp.zeshan.tolkdanmark.logik.Fragmentmanager;
@@ -32,7 +33,7 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
     public static JSONObject object;
     private Button next = null;
     private String s1, s2, s3, s4;
-    private int[] val4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+    private int[] val4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         View rod = i.inflate(R.layout.tolkbilag2_view, container, false);
@@ -45,8 +46,8 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
         textView2 = (TextView) rod.findViewById(R.id.textView2); //Slettes senere, det er kun for at test
         sprog = (EditText) rod.findViewById(R.id.Sprog);
         dato = (EditText) rod.findViewById(R.id.Dato);
-        fratid = (EditText) rod.findViewById(R.id.Fratid);
-        tiltid = (EditText) rod.findViewById(R.id.Tiltid);
+        fratid = (EditText) rod.findViewById(R.id.startTid);
+        tiltid = (EditText) rod.findViewById(R.id.slutTid);
 
         try {
             dato.setText(object.getString("order_date"));
@@ -58,13 +59,15 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
         }
 
         String[] tolkforbindelse = new String[]{
+                "Valg",
                 "Ambulant besøg",
                 "Førstegangstolkning under ét indlæggelsesforløb",
                 "Senere tolkning under ét índlæggelsesfprløb"
         };
-        final int[] val1 = {0, 1, 2};
+        final int[] val1 = {0, 1, 2, 3};
 
         String[] ydelsensomfang = new String[]{
+                "Valg",
                 "Planlagt tolkning 08-17 hverdage",
                 "Planlagt tolkning 17-08 hverdage",
                 "Akuttolkning 08-17 hverdage",
@@ -74,14 +77,15 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
                 "Tolken udeblevet",
                 "Tolkning aflyst indenfor 12 timer"
         };
-        final int[] val2 = {0, 1, 2, 3, 4, 5, 6, 7};
+        final int[] val2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
         String[] ydelsenstype = new String[]{
+                "Valg",
                 "Konsultation",
                 "Telefonkonsultation",
                 "Webcamtolkning"
         };
-        final int[] val3 = {0, 1, 2};
+        final int[] val3 = {0, 1, 2, 3};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, tolkforbindelse);
         tolkningtype.setAdapter(adapter);
@@ -131,7 +135,13 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == next) {
+                if (v == next && s1 == "0") {
+                    Toast.makeText(getActivity(), "Vælge venligst tolkforbindelse", Toast.LENGTH_SHORT).show();
+                } else if (v == next && s2 == "0") {
+                    Toast.makeText(getActivity(), "Vælge venligst ydelsensomfang", Toast.LENGTH_SHORT).show();
+                } else if (v == next && s3 == "0") {
+                    Toast.makeText(getActivity(), "Vælge venligst ydelsenstype", Toast.LENGTH_SHORT).show();
+                } else {
                     getFragmentManager().beginTransaction().replace(R.id.container, fragments.getTolkbilag3fragment()).addToBackStack(fragments.getTolkbilag3fragment().getTag()).commit();
                 }
             }
@@ -171,77 +181,77 @@ public class Tolkbilag2_fragment extends Fragment implements Datovaelger_Fragmen
     }
 
     public void valueUpdate() {
-        if (s2 == "0" && s3 == "0") {
-            s4 = String.valueOf(val4[0]);
-            textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "0" && s3 == "1") {
+        if (s2 == "1" && s3 == "1") {
             s4 = String.valueOf(val4[1]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "0" && s3 == "2") {
+        } else if (s2 == "1" && s3 == "2") {
             s4 = String.valueOf(val4[2]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "1" && s3 == "0") {
+        } else if (s2 == "1" && s3 == "3") {
             s4 = String.valueOf(val4[3]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "1" && s3 == "1") {
+        } else if (s2 == "2" && s3 == "1") {
             s4 = String.valueOf(val4[4]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "1" && s3 == "2") {
+        } else if (s2 == "2" && s3 == "2") {
             s4 = String.valueOf(val4[5]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "2" && s3 == "0") {
+        } else if (s2 == "2" && s3 == "3") {
             s4 = String.valueOf(val4[6]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "2" && s3 == "1") {
+        } else if (s2 == "3" && s3 == "1") {
             s4 = String.valueOf(val4[7]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "2" && s3 == "2") {
+        } else if (s2 == "3" && s3 == "2") {
             s4 = String.valueOf(val4[8]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "3" && s3 == "0") {
+        } else if (s2 == "3" && s3 == "3") {
             s4 = String.valueOf(val4[9]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "3" && s3 == "1") {
+        } else if (s2 == "4" && s3 == "1") {
             s4 = String.valueOf(val4[10]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "3" && s3 == "2") {
+        } else if (s2 == "4" && s3 == "2") {
             s4 = String.valueOf(val4[11]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "4" && s3 == "0") {
+        } else if (s2 == "4" && s3 == "3") {
             s4 = String.valueOf(val4[12]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "4" && s3 == "1") {
+        } else if (s2 == "5" && s3 == "1") {
             s4 = String.valueOf(val4[13]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "4" && s3 == "2") {
+        } else if (s2 == "5" && s3 == "2") {
             s4 = String.valueOf(val4[14]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "5" && s3 == "0") {
+        } else if (s2 == "5" && s3 == "3") {
             s4 = String.valueOf(val4[15]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "5" && s3 == "1") {
+        } else if (s2 == "6" && s3 == "1") {
             s4 = String.valueOf(val4[16]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "5" && s3 == "2") {
+        } else if (s2 == "6" && s3 == "2") {
             s4 = String.valueOf(val4[17]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "6" && s3 == "0") {
+        } else if (s2 == "6" && s3 == "3") {
             s4 = String.valueOf(val4[18]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "6" && s3 == "1") {
+        } else if (s2 == "7" && s3 == "1") {
             s4 = String.valueOf(val4[19]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "6" && s3 == "2") {
+        } else if (s2 == "7" && s3 == "2") {
             s4 = String.valueOf(val4[20]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "7" && s3 == "0") {
+        } else if (s2 == "7" && s3 == "3") {
             s4 = String.valueOf(val4[21]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "7" && s3 == "1") {
+        } else if (s2 == "8" && s3 == "1") {
             s4 = String.valueOf(val4[22]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
-        } else if (s2 == "7" && s3 == "2") {
+        } else if (s2 == "8" && s3 == "2") {
             s4 = String.valueOf(val4[23]);
+            textView2.setText(s4); //Slettes senere, det er kun for at test
+        } else if (s2 == "8" && s3 == "3") {
+            s4 = String.valueOf(val4[24]);
             textView2.setText(s4); //Slettes senere, det er kun for at test
         }
     }
